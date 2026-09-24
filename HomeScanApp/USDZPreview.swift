e7d83164055a570eaf663ab9500a1f4c0d9f1825
TheTobeQ <bobidonebizness@gmail.com>
2026-09-24 12:52:38 +0200

    Add interactive USDZ preview

---
+++ HomeScanApp/USDZPreview.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+import QuickLook
+import UIKit
+
+struct USDZPreview: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: Context) -> QLPreviewController {
+        let controller = QLPreviewController()
+        controller.dataSource = context.coordinator
+        return controller
+    }
+
+    func updateUIViewController(_ controller: QLPreviewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(url: url)
+    }
+
+    final class Coordinator: NSObject, QLPreviewControllerDataSource {
+        let url: URL
+
+        init(url: URL) {
+            self.url = url
+        }
+
+        func numberOfPreviewItems(in controller: QLPreviewController) -> Int { 1 }
+
+        func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+            url as NSURL
+        }
+    }
+}
